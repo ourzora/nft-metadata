c07409069a7c395f6a979cec1802e7525830b995
Ethan Daya <me@Ethandaya.com>
2021-07-17 23:35:19 +1000

    feat: add catalog support

--- src/parsers/zoraMetadataParser.ts
+++ src/parsers/zoraMetadataParser.ts
@@ -3,6 +3,25 @@ import { MediaFactory } from '@zoralabs/core/dist/typechain'
 import { fetchMetadata, fetchMimeType } from '../utils/fetch'
 import { ParserConfig, ParserResponse } from './index'
 
+export function translateMetadataSchema(schema: any) {
+  if ('body' in schema) {
+    return {
+      origin: 'catalog-20210202',
+      version: 'zora-20210604',
+      name: schema.body.title,
+      description: schema.body.notes || '',
+      mimeType: schema.body.mimeType,
+      image: !schema.body?.artwork?.isNft
+        ? schema.body?.artwork?.info?.uri
+        : undefined,
+      external_url: 'https://catalog.works',
+      ...schema,
+    }
+  }
+
+  return schema
+}
+
 export async function parseZoraMetadata({
   fetchTimeout,
   provider,
@@ -22,9 +41,21 @@ export async function parseZoraMetadata({
     const ZContract = MediaFactory.connect(contractAddress, provider)
     const contentURI = await ZContract.tokenURI(tokenId)
     const contentURL = getIPFSUrl(contentURI, ipfsBaseURL)
-    const fetchContentURL = getIPFSUrl(tokenURI, ipfsBaseURL, true)
+    const fetchContentURL = getIPFSUrl(contentURI, ipfsBaseURL, true)
 
-    const { name, description, externalURL, mimeType } = metadata
+    const translatedMeta = translateMetadataSchema(metadata)
+
+    console.log(JSON.stringify(translatedMeta, null, 2))
+
+    const { name, description, externalURL, mimeType, image } = translatedMeta
+
+    const imageURL = image ? getIPFSUrl(image, ipfsBaseURL) : undefined
+    const imageURLMimeType = imageURL
+      ? await fetchMimeType(imageURL, {
+          timeout: fetchTimeout,
+        })
+      : undefined
+
     const contentURLMimeType = mimeType
       ? mimeType
       : await fetchMimeType(fetchContentURL, {
@@ -37,6 +68,8 @@ export async function parseZoraMetadata({
       tokenURLMimeType: contentType,
       contentURL,
       contentURLMimeType,
+      ...(imageURL && { imageURL }),
+      ...(imageURLMimeType && { imageURLMimeType }),
       ...(name && { name }),
       ...(description && { description }),
       ...(externalURL && { externalURL }),
