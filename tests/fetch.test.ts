4df4f18ac212dafc9853588fccb0e2a3604960c5
Iain <iain@zora.co>
2021-10-25 16:27:08 -0400

    update data-uri parsing for semi-malformed non-encoded data-uris

---
+++ tests/fetch.test.ts
@@ -0,0 +1,25 @@
+import { parseDataUri } from '../src'
+
+describe('fetch.ts', () => {
+  describe('parses data-uris', () => {
+    test.each`
+      tag                                         | input                                                                                           | mime                     | expectedResult
+      ${'handles a standard text data uri'}       | ${'data:text/plain,hello world'}                                                                | ${'text/plain'}          | ${'hello world'}
+      ${'handles a semi-malfomred svg data uri'}  | ${'data:application/svg+xml,<svg><rect x="0" y="0" /></svg>'}                                   | ${'application/svg+xml'} | ${'<svg><rect x="0" y="0" /></svg>'}
+      ${'handles a base64-encoded svg data uri'}  | ${'data:application/svg+xml;charset=utf-8;base64,PHN2Zz48cmVjdCB4PSIwIiB5PSIwIiAvPjwvc3ZnPg=='} | ${'application/svg+xml'} | ${'<svg><rect x="0" y="0" /></svg>'}
+      ${'handles a json data uri'}                | ${'data:application/json,{"name": "test"}'}                                                     | ${'application/json'}    | ${'{"name": "test"}'}
+      ${'handles a base64-encoded json data uri'} | ${'data:application/json;base64,eyJuYW1lIjogInRlc3QifQ=='}                                      | ${'application/json'}    | ${'{"name": "test"}'}
+    `(
+      '$tag: parses data uri correctly',
+      ({ input, expectedResult, mime }: any) => {
+        const dataUri = parseDataUri(input)
+        if (!dataUri) {
+          throw new Error('missing data uri parse')
+        }
+
+        expect(dataUri.body).toEqual(expectedResult)
+        expect(dataUri.mime).toEqual(mime)
+      },
+    )
+  })
+})
