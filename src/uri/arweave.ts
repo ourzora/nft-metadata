670e4e029b80cb66249d679f4d2aa193b0b06aad
Iain Nash <iain@zora.co>
2021-11-30 19:03:33 -0500

    Support ar uris (#28)
    * support ar:// uris
    
    * fix tests import paths

---
+++ src/uri/arweave.ts
@@ -0,0 +1,22 @@
+import { ARWEAVE_DEFAULT } from "../constants/providers";
+import { isValidHttpUrl } from "./fetch";
+
+
+export function isArweave(uri: string) {
+  const hasPrefix = uri.startsWith('ar://')
+  return uri.startsWith('https://arweave.net/') || hasPrefix;
+}
+
+export function hasArPrefix(uri: string) {
+  return uri.startsWith('ar://')
+}
+
+export function getARWeaveURI(uri: string) {
+  if (hasArPrefix(uri)) {
+    return uri.replace('ar://', ARWEAVE_DEFAULT)
+  }
+  if (isValidHttpUrl(uri)) {
+    return uri;
+  }
+  throw new Error('Cannot parse ARWeave URI');
+}
