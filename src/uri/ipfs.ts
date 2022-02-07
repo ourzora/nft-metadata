c3f69f31a7fcfa68d6936a9928aff5a4c13c1b74
Iain <iain@zora.co>
2022-02-07 11:35:54 -0500

    remove dep on pinata gateway tools

--- src/uri/ipfs.ts
+++ src/uri/ipfs.ts
@@ -1,26 +1,14 @@
 import { isAddressMatch } from '../utils/addresses'
+import { getCID, convertToDesiredGateway } from '../utils/gateway-tools'
 import {
   FOUNDATION_TOKEN_ADDRESS,
   MAKERSPLACE_TOKEN_ADDRESS,
   ZORA_TOKEN_ADDRESS,
 } from '../constants/addresses'
 
-const GatewayTools = require('../utils/gateway-tools')
-
-export function isIPFS(uri: string) {
-  const result = GatewayTools.containsCID(uri)
-
-  // TODO - this lib is not working right so hack for now
-  return result.containsCid && (uri.includes('/ipfs/') || hasIpfsPrefix(uri))
-}
-
-export function hasIpfsPrefix(uri: string) {
-  return uri.startsWith('ipfs://')
-}
-
 export function getIPFSUrl(uri: string, gateway: string) {
-  if (isIPFS(uri)) {
-    return GatewayTools.convertToDesiredGateway(uri, gateway)
+  if (getCID(uri)) {
+    return convertToDesiredGateway(uri, gateway)
   }
   return uri
 }
--- src/utils/gateway-tools.ts
+++ src/utils/gateway-tools.ts
@@ -1,67 +1,59 @@
-const isIPFS = require('is-ipfs')
+import isIPFS from 'is-ipfs'
 
 /* 
-From https://github.com/PinataCloud/ipfs-gateway-tools/blob/master/src/index.js
-Package was broken on minor upgrade, so moving code to utility file and making typescript.
+  From https://github.com/PinataCloud/ipfs-gateway-tools/blob/master/src/index.js
+  Package was broken on minor version that was not published to github.
+  Moving code to utility file and cleaning up usage/syntax.
 */
 
-export function containsCID(url: string) {
+export function getCID(url: string) {
   if (typeof url !== 'string') {
     throw new Error('url is not string')
   }
   const splitUrl = url.split('/')
   for (const split of splitUrl) {
     if (isIPFS.cid(split)) {
-      return {
-        containsCid: true,
-        cid: split,
-      }
+      return split
     }
     const splitOnDot = split.split('.')[0]
     if (isIPFS.cid(splitOnDot)) {
-      return {
-        containsCid: true,
-        cid: splitOnDot,
-      }
+      return splitOnDot
     }
   }
 
-  return {
-    containsCid: false,
-    cid: null,
-  }
+  return null
 }
 
-
-export const convertToDesiredGateway = (sourceUrl: string, desiredGatewayPrefix: string) => {
-  const results = containsCID(sourceUrl);    
-  if (results.containsCid !== true || !results.cid) {
-    throw new Error("url does not contain CID");
+export const convertToDesiredGateway = (
+  sourceUrl: string,
+  desiredGatewayPrefix: string,
+) => {
+  const cid = getCID(sourceUrl)
+  if (!cid) {
+    throw new Error('url does not contain CID')
   }
 
-  if(isIPFS.cid(results.cid)) {
-    return `${desiredGatewayPrefix}/ipfs/${results.cid}`
+  if (isIPFS.cid(cid)) {
+    return `${desiredGatewayPrefix}/ipfs/${cid}`
   }
 
-  const splitUrl = sourceUrl.split(results.cid);
+  const splitUrl = sourceUrl.split(cid)
 
   // Case 1 - the ipfs://cid path
-  if (sourceUrl.includes(`ipfs://${results.cid}`)) {
-    return `${desiredGatewayPrefix}/ipfs/${results.cid}${splitUrl[1]}`;
+  if (sourceUrl.includes(`ipfs://${cid}`)) {
+    return `${desiredGatewayPrefix}/ipfs/${cid}${splitUrl[1]}`
   }
 
   // Case 2 - the /ipfs/cid path (this should cover ipfs://ipfs/cid as well
-  if (sourceUrl.includes(`/ipfs/${results.cid}`)) {
-    return `${desiredGatewayPrefix}/ipfs/${results.cid}${splitUrl[1]}`;
+  if (sourceUrl.includes(`/ipfs/${cid}`)) {
+    return `${desiredGatewayPrefix}/ipfs/${cid}${splitUrl[1]}`
   }
 
   // Case 3 - the /ipns/cid path
-  if (sourceUrl.includes(`/ipns/${results.cid}`)) {
-    return `${desiredGatewayPrefix}/ipns/${results.cid}${splitUrl[1]}`;
+  if (sourceUrl.includes(`/ipns/${cid}`)) {
+    return `${desiredGatewayPrefix}/ipns/${cid}${splitUrl[1]}`
   }
 
   // This is the fallback if no supported patterns are provided
-  throw new Error(
-    "Unsupported IPFS URL pattern"
-  );
-};
+  throw new Error('Unsupported IPFS URL pattern')
+}
